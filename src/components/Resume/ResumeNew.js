e560f0d3f9fb8740b3cc16bec130170ef7faa62f
Newton1037 <nitinkumar1037nk@gmail.com>
2024-08-15 22:17:32 +0530

    pd

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/9921103019_NITIN KUMAR.pdf"; 
+import pdf from "../../Assets/9921103019_Nitin Kumar.pdf"; 
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
